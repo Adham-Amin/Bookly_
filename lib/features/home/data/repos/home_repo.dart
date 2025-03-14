69e65c26bbb39cf8dc4ebb4605b940f0a2023cf4
Adham-Amin <adhm0amin@gmail.com>
2025-03-14 23:58:02 +0200

    Create Home Repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/error/failures.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFreeBooks();
+}
